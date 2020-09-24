5a4fd1158bd0a103ae742e271e5e5a7609e1cd5b
kunal phaltankar <kunalphaltankar@gmail.com>
2020-09-25 00:18:39 +0530

    Updates

---
+++ precache-manifest.c02341334ce145ba533a6cc82a68c998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa0b185ea6dbe1a3554f56ee3568de2",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "d8cb7e08079252d3a1f6",
+    "url": "/sudoku/static/css/main.86dd248c.chunk.css"
+  },
+  {
+    "revision": "5eab2a9669ea7bba259c",
+    "url": "/sudoku/static/js/2.cc5d0aab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku/static/js/2.cc5d0aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8cb7e08079252d3a1f6",
+    "url": "/sudoku/static/js/main.eafb6133.chunk.js"
+  },
+  {
+    "revision": "8d8e892f47ac5d31712b",
+    "url": "/sudoku/static/js/runtime-main.f7976529.js"
+  }
+]);
